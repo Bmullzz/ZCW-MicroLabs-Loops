ab7c6a4a6693d023dd2d9b4df41cd5a4d8b3fd18
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-09 17:01:09 -0400

    Finish the car ride. We there

--- src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/CarRide.java
@@ -1,9 +1,40 @@
 package io.zipcoder.microlabs.mastering_loops;
+import java.util.Scanner;
 
 public class CarRide {
 
-    public String areWeThereYetTest(){
-        return "";
+    Scanner input = new Scanner(System.in);
+
+    public String areWeThereYet(){
+
+        //System.out.println("Are we there yet?");
+
+        boolean answer = false;
+        String userInput;
+
+        while(answer != true){
+
+            System.out.println("Are we there yet?");
+            userInput = input.nextLine();
+
+            if(userInput.equals("yes")) break;
+            if(userInput.equals("Yes")) break;
+            if(userInput.equals("YES")) break;
+            if(userInput.equals("Yes!")) break;
+            if(userInput.equals("YES!")) break;
+            if(userInput.equals("Yes.")) break;
+            if(userInput.equals("Yea")) break;
+
+
+
+        }
+
+        return "Good!";
+    }
+
+    public static void main(String[] args) {
+        CarRide carRide = new CarRide();
+        System.out.println(carRide.areWeThereYet());
     }
 
 }
--- src/test/java/io/zipcoder/microlabs/mastering_loops/CarRideTest.java
+++ src/test/java/io/zipcoder/microlabs/mastering_loops/CarRideTest.java
@@ -1,9 +1,21 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CarRideTest {
+    @Test
+    public void oneToTenTest(){
+        //: Given
+        CarRide ride = new CarRide();
+        String expected = "*** Output ***\noneToTen()\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10";
 
+        //: When
+        String actual =ride.areWeThereYet();
+
+        //: Then
+        Assert.assertEquals("The two strings should be equal", expected, actual);
+    }
 
 }
