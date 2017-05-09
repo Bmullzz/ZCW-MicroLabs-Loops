f0c01279855771f41f11c8d86f8ce2bdcc5f60cb
Brian Mullin <brianmullin@zipcoder6.local>
2017-05-09 17:30:45 -0400

    Made a triangle

--- src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/Shapes.java
@@ -3,8 +3,17 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class Shapes {
 
-    public String triangle(){
-        return "";
+
+    public void triangle(int n){
+
+        for (int i=0; i<=n; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+
+        }
+
     }
 
     public String tableSquare(){
@@ -13,5 +22,10 @@ public class Shapes {
 
     public String tableSquares(int n){
         return "";
+    }
+
+    public static void main(String[] args) {
+        Shapes shape = new Shapes();
+        shape.triangle(9);
     }
 }
